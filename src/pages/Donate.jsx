4599f25a704214eff04bec57b565f6d64189b4a7
Raphael Bajet <baphael@berkeley.edu>
2024-11-14 17:44:48 -0800

    donate page, centering and accounting for header height

--- src/pages/Donate.jsx
+++ src/pages/Donate.jsx
@@ -7,8 +7,17 @@ const Donate = () => {
     return (
     <>
     <Header currentPage="donate"/>
-    <section>
-        
+    <section className="donate">
+        <div className="donate-content">
+            <div className="explanatory-text">
+                <p id="header">Donate</p>
+                <p>Because our services are pro bono, we appreciate any and all support! Donations go towards travel reimbursements for our volunteers, honoraria for our guest speakers, and other operational expenses</p>
+            </div>
+            <div className="button-div">
+                <Button buttonText="DONATE TO VITA" linkTo="https://www.youtube.com/watch?v=dQw4w9WgXcQ" />
+                <p>Under Student Organization Name, please enter: “Volunteer Income Tax Assistance Program”</p>
+            </div>
+        </div>
     </section>
     <Footer />
     </>
