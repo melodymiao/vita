a9262e59e363e9d056d62923c5fa4a89dc8d406c
Raphael Bajet <baphael@berkeley.edu>
2024-11-14 16:56:14 -0800

    donate button is now different style while on donate page

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -9,20 +9,20 @@ const Header = ({ currentPage }) => {
       <section className='header-section'>
         <img src='logo-campanile-i.png' className='logo' />
         <div className='nav-buttons'>
-          <Link to="/" className='nav-button'>
+          <Link to="/" className={`nav-button ${currentPage === "home" ? "active" : ""}`} id="home">
             <div className='nav-text'>Home</div>
             <div className="underline"></div>
           </Link>
-          <Link to="/free-tax-filing" className='nav-button'>
+          <Link to="/free-tax-filing" className={`nav-button ${currentPage === "free-tax-filing" ? "active" : ""}`} id="free-tax-filing">
             <div className='nav-text'>Free Tax Filing</div>
             <div className="underline"></div>
           </Link>
-          <Link to="/decal" className='nav-button'>
+          <Link to="/decal" className={`nav-button ${currentPage === "decal" ? "active" : ""}`} id="donate">
             <div className='nav-text'>Decal</div>
             <div className="underline"></div>
           </Link>
 
-          <Link to="/donate" id="donate-button2">Donate</Link>
+          <Link to="/donate" id={`${currentPage === "donate" ? "donate-button" : "donate-button2"}`}>Donate</Link>
         </div>
       </section>
     </>
